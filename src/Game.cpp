46e6318982f77d078ecb5bed281e878ca5d6d66d
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2023-12-16 20:58:18 +0100

    .

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Game.h"
+
+Game::Game() {
+    for (int col {0}; col < size; col++)
+        for (int row {0}; row < size; row++)
+            board.push_back('_');
+    SetToken(size / 2 - 1, size / 2 - 1, 'O');
+    SetToken(size / 2, size / 2, 'O');
+    SetToken(size / 2 - 1, size / 2, 'X');
+    SetToken(size / 2, size / 2 - 1, 'X');
+};
--- src/Game.h
+++ src/Game.h
@@ -4,15 +4,18 @@
 
 #ifndef GAME_H
 #define GAME_H
-
+#include <vector>
 
 
 class Game {
 public:
+    Game();
     int GetSize() const { return size; }
+    void SetToken(int col, int row, char token){ board[size * row + col] = token;}
+    char GetToken(int col, int row){ return board[size * row + col];}
 private:
     int size {8};
-
+    std::vector<char> board;
 };
 
 
