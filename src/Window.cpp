3c14f4037d2451cde88ce317db8e2afc1fc68f0a
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2024-09-30 22:52:40 +0200

    .

--- src/Window.cpp
+++ src/Window.cpp
@@ -5,16 +5,9 @@
 #include "Window.h"
 
 LRESULT Window::WindowProc(HWND hWnd, const UINT message, const WPARAM wParam, const LPARAM lParam) {
-    LONG_PTR window_long_ptr = GetWindowLongPtr(hWnd, 0);
-    if (window_long_ptr) {
-        return ((Window*) window_long_ptr)->wnd_proc(message, wParam, lParam);
-        /*
-        const auto window = reinterpret_cast<Window *>(window_long_ptr);
-        if (window) {
-            return window->wnd_proc(message, wParam, lParam);
-        }
-    */
+    if (const auto window_long_ptr {GetWindowLongPtr(hWnd, 0)}) {
+        return reinterpret_cast<Window*>(window_long_ptr)->wnd_proc(message, wParam, lParam);
     }
-    return DefWindowProc(hWnd, message, wParam, lParam);
+    return DefWindowProcW(hWnd, message, wParam, lParam);
 }
 
